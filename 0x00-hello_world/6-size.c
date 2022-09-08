1f428c2b5d9a341858297a73f4000652397f1a66
Gabriel <gabbydamian92@gmail.com>
2022-09-08 07:53:42 -0700

    Issues with 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
 {
-	int 
+	int integer;
+	long int longInteger;
+	long long int longLongInteger;
+	char character;
+	float floatNumber;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longInteger));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longLongInteger));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatNumber));
+	return (0);
+}
