2a915781c738713ee6286d95ec45c2cbe7aaedaf
Gabriel <gabbydamian92@gmail.com>
2022-09-09 06:00:33 -0700

    tasks 5&6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * function receives no parameters
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar("\n");
+	return (0);
+}
