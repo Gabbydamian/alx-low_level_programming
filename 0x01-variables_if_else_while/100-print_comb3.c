6c3d1159ca5ca2c8bd7993aee183c9e11fa69e17
Gabriel <gabbydamian92@gmail.com>
2022-09-09 06:49:54 -0700

    two of the advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point for program
+ * Return: 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b;
+
+	while (a <= '9')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			if (a == '8' && b == '9')
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++
+	}
+	return (0);
+}
