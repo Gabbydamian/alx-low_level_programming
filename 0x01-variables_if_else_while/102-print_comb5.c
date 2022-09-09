25a94f4db00be35860765ad6950ea9004a6b4dff
Gabriel <gabbydamian92@gmail.com>
2022-09-09 07:07:34 -0700

    fixed 102 I think

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,23 +12,23 @@ int main(void)
 	{
 		b = a + 1;
 		while (b <= 99)
+		{
+			putchar(a / 10 % 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 % 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 98 && b == 99)
 			{
-				putchar(a / 10 % 10 + '0');
-				putchar(a % 10 + '0');
-				putchar(' ');
-				putchar(b / 10 % 10 + '0');
-				putchar(b % 10 + '0');
-				if (a == 98 && b == 99)
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				b++;
+				putchar('\n');
 			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
 		a++;
 	}
 	return (0);
