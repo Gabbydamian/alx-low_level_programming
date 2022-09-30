4d047df2772a0789b65ae2c3b42ab39771f25bc7
Gabriel <gabbydamian92@gmail.com>
2022-09-30 12:43:55 -0700

    fixed puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
-void _puts(char *s)
+/**
+ *  * _puts - prints a string to stdout, followed by a neline
+ *   * @str: string of choice
+ *    * Return: void
+ *     */
+
+void _puts(char *str)
 {
+		int i;
+
+			for (i = 0; str[i] != '\0'; i++)
+					{
+								_putchar(str[i]);
+									}
+				_putchar('\n');
 }
