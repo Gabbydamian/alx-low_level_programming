d9ee2fde00670cf25a579f270fbab89d98da9c11
Gabriel <gabbydamian92@gmail.com>
2022-10-03 05:31:16 -0700

    mandatory tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: number of array items
+ * @c: content of array
+ * Return: a char size
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
