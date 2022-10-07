f4fa9029bfab448abecdd58baf27c9c27304ac73
Gabriel <gabbydamian92@gmail.com>
2022-10-07 03:30:54 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,41 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - concats two strings
- * @s2: string 1
- * @s1: string2
- * @n: integer
- * Return: string
- */
+* string_nconcat - concatentates two strings using the newly allocated memory
+* @s1: first string
+* @s2: second string
+* @n: unsigned int
+* Return: a pointer to 2 string concat..
+**/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int l = 0, i = 0;
-	unsigned int j = 0;
+	int l, i;
+	unsigned int j;
 	char *p;
+
+	l = 0;
+	i = 0;
+	j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[1])
+	while (s1[l])
 		l += 1;
-	p = malloc(1 + n + 1);
+	p = malloc(l + n + 1);
 	if (p == NULL)
 		return (NULL);
 	while (s1[i])
+	{
+		p[i] = s1[i];
+		i += 1;
+	}
+
+	while (j < n)
 	{
 		p[i + j] = s2[j];
 		j += 1;
